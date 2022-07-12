7a0a8792930248b6eb368ebbfb99ce051a2bf4d8
Abdullah Alnutayfi <ccsi-iuni@hotmail.com>
2022-07-12 17:40:58 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import API_KEY from './keys';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg'
+const App = () => {
+  const APY_URL = `http://www.omdbapi.com?apikey=${API_KEY}`
+  const [movies, setMovies] = useState([])
+  const [title, setTitle] = useState('')
+ 
+  const fetchData = async (title) => {
+    const response = await fetch(`${APY_URL}&s=${title}`)
+    const data = await response.json();
+    console.log(data.search);
+    setMovies(data.Search);
+  }
+  useEffect(() => {
+    fetchData('all');
+  }, []);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='header'>
+        <h1>Movies</h1>
+      </div>
+      <div className='search'>
+        <input
+          placeholder='Search'
+          value={title}
+          onChange={(seartTerm) => setTitle(seartTerm.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => fetchData(title)} />
+      </div>
+        {
+          movies.length > 0 ?
+          <div className='container'>
+            {
+              movies.map((movie) => <MovieCard movie={movie}/>)
+            }
+          </div>
+          :(
+            <div className='empty'>
+              <h3>No Movies</h3>
+            </div>
+          )
+        }
     </div>
   );
 }
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+const API_KEY = '9860171b'
+export default API_KEY;
